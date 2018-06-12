60bf2a1c8b037b2a056848fe95ced1644edbc901
Phyoeieibo <phyoei083@gmail.com>
2018-06-12 14:36:53 +0630

    commit update

---
+++ greeting.py
@@ -0,0 +1,9 @@
+from flask import Flask
+a=Flask(__name__)
+@a.route("/")
+def greeting():
+    return "Hello My name is Phyo Ei!"
+@a.route("/tiide")
+def tiide():
+    return "Welcome from my first Heroku Page"
+
